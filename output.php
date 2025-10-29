1b3b7ff41b5855c3ab2cd1015344d033b6a53a8e
unknown <your@email.example>
2025-10-29 09:33:28 +0200

    added name output in terminal

---
+++ output.php
@@ -0,0 +1,18 @@
+<?php
+
+// 1. uzdevums: Vides sagatavošana
+// a) Iztīriet sava vārda mapi tukšu;
+// b) Atveriet vārda mapi Terminal aplikācijā;
+// c) Izveidojiet mapi "js-formatīvais";
+// d) Atveriet mapes saturu ar VS Code;
+// e) Izveidojiet failu "output.js", kurā ir koda rinda, kas konsolē izvada Tavu vārdu;
+// f) Izpildiet šo failu ar "node" programmu un pārliecinaties, ka vārds ir izvadīts;
+
+
+
+// 2. uzdevums: Versionēšana
+// a) Versionējiet "js-formatīvais" mapi ar Git versiju kontroli izveidojot pirmo komitu (commit);
+// b) Izveidojiet repozitoriju ar tādu pašu nosaukumu savā Github vietnē;
+// c) Savienojiet lokālo repozitoriju ar Github repozitoriju un nosūtiet komitu uz serveri;
+
+echo "Jānis";
