17f9feecee57d063a318e2e6b07b07e407f60704
unknown <your@email.example>
2025-10-29 10:02:53 +0200

    bugfix update

--- output.php
+++ output.php
@@ -1,11 +1,13 @@
 <?php
 
 echo "Jānis";
+echo "---";
 $groupName = "PT2025"; // tas pats, ka let js
 $size = 23; // tas pats, ka let js
 $isActive = true;  // tas pats, ka let js
 
 
 define("CONSTANT", "Hello world."); //nozime ka Constant =  Hello, World.
-echo CONSTANT; // Выводит "Hello world."
+echo CONSTANT; // Выводит "Hello world.
+echo "---";
 echo $groupName;
