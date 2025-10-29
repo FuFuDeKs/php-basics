5e6bfd62535f6e2242523a819f9da99a77c17d1b
unknown <your@email.example>
2025-10-29 09:46:18 +0200

    added basic logic

--- output.php
+++ output.php
@@ -1,18 +1,11 @@
 <?php
 
-// 1. uzdevums: Vides sagatavošana
-// a) Iztīriet sava vārda mapi tukšu;
-// b) Atveriet vārda mapi Terminal aplikācijā;
-// c) Izveidojiet mapi "js-formatīvais";
-// d) Atveriet mapes saturu ar VS Code;
-// e) Izveidojiet failu "output.js", kurā ir koda rinda, kas konsolē izvada Tavu vārdu;
-// f) Izpildiet šo failu ar "node" programmu un pārliecinaties, ka vārds ir izvadīts;
-
-
-
-// 2. uzdevums: Versionēšana
-// a) Versionējiet "js-formatīvais" mapi ar Git versiju kontroli izveidojot pirmo komitu (commit);
-// b) Izveidojiet repozitoriju ar tādu pašu nosaukumu savā Github vietnē;
-// c) Savienojiet lokālo repozitoriju ar Github repozitoriju un nosūtiet komitu uz serveri;
-
 echo "Jānis";
+$groupName = "PT2025"; // tas pats, ka let js
+$size = 23; // tas pats, ka let js
+$isActive = true;  // tas pats, ka let js
+
+
+define("CONSTANT", "Hello world."); //nozime ka Constant =  Hello, World.
+echo CONSTANT; // Выводит "Hello world."
+echo $groupName;
